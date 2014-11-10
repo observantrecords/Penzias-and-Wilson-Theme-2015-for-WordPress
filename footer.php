9bc4a1e68135a559e70403c907076ac32bc1504f
Greg Bueno <greg@gregbueno.com>
2014-11-10 14:18:23 -0800

    Adding to source control.

---
+++ footer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gregbueno
+ * Date: 11/9/14
+ * Time: 10:44 AM
+ */
+
+namespace ObservantRecords\WordPress\Themes\PenziasAndWilson;
+
+?>
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/dark_balcony-half.jpg" class="bg" />
+
+	</div>
+
+	<?php wp_footer(); ?>
+
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+			var pageTracker = _gat._getTracker("UA-7828220-2");
+			pageTracker._trackPageview();
+		} catch(err) {}
+	</script>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gregbueno
+ * Date: 11/9/14
+ * Time: 10:54 AM
+ */
+
+namespace ObservantRecords\WordPress\Themes\PenziasAndWilson {
+	const WP_TEXT_DOMAIN = 'penziasandwilson';
+}
+
+
