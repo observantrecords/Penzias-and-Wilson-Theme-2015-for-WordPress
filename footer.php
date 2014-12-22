6fc4c582d7df35b6eb158c0516418587f2b6bceb
Greg Bueno <nemesisvex@outlook.com>
2014-12-22 10:55:29 -0800

    Refactoring header and replacing Google Analytics code.

--- footer.php
+++ footer.php
@@ -15,16 +15,5 @@ namespace ObservantRecords\WordPress\Themes\PenziasAndWilson;
 
 	<?php wp_footer(); ?>
 
-	<script type="text/javascript">
-		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-		try {
-			var pageTracker = _gat._getTracker("UA-7828220-2");
-			pageTracker._trackPageview();
-		} catch(err) {}
-	</script>
-
 </body>
 </html>
