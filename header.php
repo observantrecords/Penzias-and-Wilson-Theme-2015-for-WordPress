9c8779d78ea445c22d2d9efa115713aaa2ed450c
Greg Bueno <nemesisvex@outlook.com>
2015-11-04 13:37:07 -0800

    Adding favicon to header.php.

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ namespace ObservantRecords\WordPress\Themes\PenziasAndWilson;
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon">
     <?php wp_head(); ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
